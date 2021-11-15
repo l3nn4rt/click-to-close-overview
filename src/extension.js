94d2037091b294d1c276bd8d37d499d0c0b47a33
l3nn4rt <l3nn4rt@protonmail.com>
2021-11-15 23:50:00 +0100

    Port to Gnome Shell >= 40
    Compatibility with previous versions is broken.

--- src/extension.js
+++ src/extension.js
@@ -30,109 +30,67 @@ const Me = ExtensionUtils.getCurrentExtension();
 const GSCHEMA = 'org.gnome.shell.extensions.click-to-close-overview';
 
 class ClickToCloseOverview {
-	constructor() {
-		let gschema = Gio.SettingsSchemaSource.new_from_directory(
-			Me.dir.get_child('schemas').get_path(),
-			Gio.SettingsSchemaSource.get_default(),
-			false
-		);
-
-		this.settings = new Gio.Settings({
-			settings_schema: gschema.lookup(GSCHEMA, true)
-		});
-
-		this.closeAppsPage = () => {
-			Main.overview.viewSelector._appsPage.hide();
-			Main.overview.toggle();
-			this._appsPageClosing = false;
-		};
-	}
-
 	enable() {
 		/* connections to undo when disabling go here */
 		this._connections = [];
-		/* true when the user is scrolling apps grid */
-		this._appsPageSwiping = false;
-		/* true when closing the apps page for a click */
-		this._appsPageClosing = false;
 
+		/* make the overview susceptible to clicks */
+		this._oldReactivity = Main.overview._overview._controls.reactive;
+		Main.overview._overview._controls.reactive = true;
 
-		/* connect to existing click action in workspaces page */
-		const wsDisplay = Main.overview.viewSelector._workspacesDisplay;
-		const wsCallback = wsDisplay._clickAction.connect('clicked', action => {
-			let event = Clutter.get_current_event();
-			let index = wsDisplay._getMonitorIndexForEvent(event);
-			if ((action.get_button() == 1 || action.get_button() == 0) &&
-			    !wsDisplay._workspacesViews[index].getActiveWorkspace().isEmpty())
-				Main.overview.hide();
-		});
-		this._connections.push([wsDisplay._clickAction, wsCallback]);
-
-
-		/* make the apps page susceptible to clicks */
-		this._oldAppsPageReactivity = Main.overview.viewSelector._appsPage.reactive;
-		Main.overview.viewSelector._appsPage.reactive = true;
-
-		/* create new click action for the apps page */
-		this._appsPageClickAction = new Clutter.ClickAction();
-		const appCallback = this._appsPageClickAction.connect('clicked', action => {
+		/* create new click action */
+		this._clickAaction = new Clutter.ClickAction();
+		const callback = this._clickAaction.connect('clicked', action => {
 			/* ignore non-primary clicks */
 			if (action.get_button() !== 1 && action.get_button() !== 0)
 				return;
 
-			/* don't close while animating, unless fast-app-close enabled */
-			if (Main.overview.viewSelector.appDisplay._grid._clonesAnimating.length &&
-			    !this.settings.get_boolean('fast-app-close'))
-			    return;
+			/* ignore clicks when showing other app pages */
+			const appDisplay = Main.overview._overview._controls._appDisplay;
+			const showingPrevPage = appDisplay._prevPageIndicator.visible;
+			const showingNextPage = appDisplay._nextPageIndicator.visible;
+			if (showingPrevPage || showingNextPage) {
+				/* Workaround: GS is not setting these values to false after
+				 * changing app page. */
+				appDisplay._prevPageIndicator.visible = false;
+				appDisplay._nextPageIndicator.visible = false;
+				return;
+			}
 
-			/* don't close while scrolling */
-			if (this._appsPageSwiping)
+			/* clicked actor */
+			const [x, y] = global.get_pointer();
+			const actor = global.stage.get_actor_at_pos(Clutter.PickMode.ALL, x, y);
+
+			/* ignore clicks inside search box */
+			const searchBox = Main.overview._overview._controls._searchEntry;
+			if (actor === searchBox || actor.get_parent() === searchBox
+					|| actor.get_parent().get_parent() === searchBox)
 				return;
 
-			/* prevent multiple animations when fast-app-close enabled */
-			if (this._appsPageClosing)
-				return;
-			this._appsPageClosing = true;
+			/* propagate clicks inside thumbnails box */
+			const thumbnailsBox = Main.overview._overview._controls._thumbnailsBox;
+			if (actor.get_parent().get_parent() === thumbnailsBox)
+				return actor.get_parent().activate();
 
-			/* close, and animate if animate-app-display enabled */
-			if (this.settings.get_boolean('animate-app-display'))
-				Main.overview.viewSelector.appDisplay.animate(
-					IconGrid.AnimationDirection.OUT, this.closeAppsPage);
-			else
-				this.closeAppsPage();
+			Main.overview.toggle();
 		});
-		/* connect click action to the apps page */
-		Main.overview.viewSelector._appsPage.add_action(this._appsPageClickAction);
-		this._connections.push([this._appsPageClickAction, appCallback]);
-
-		/* keep track of scrolls in the apps page */
-		const tracker = Main.overview.viewSelector.appDisplay._swipeTracker;
-		this._connections.push([
-			tracker,
-			tracker.connect('begin', () => {
-				this._appsPageSwiping = true;
-			})
-		]);
-		this._connections.push([
-			tracker,
-			tracker.connect('end', () => {
-				Mainloop.timeout_add(0, () => this._appsPageSwiping = false);
-			})
-		]);
+		/* connect click action to the overview */
+		Main.overview._overview._controls.add_action(this._clickAaction);
+		this._connections.push([this._clickAaction, callback]);
 	}
 
 	disable() {
-		/* restore apps page reactivity */
-		Main.overview.viewSelector._appsPage.reactive = this._oldAppsPageReactivity;
-		this._oldAppsPageReactivity = null;
+		/* restore overview reactivity */
+		Main.overview._overview._controls.reactive = this._oldReactivity;
+		this._oldReactivity = null;
 
 		/* disconnect callbacks */
 		this._connections.forEach(([obj, callback]) => obj.disconnect(callback));
 		this._connections = null;
 
-		/* disconnect click action from the apps page */
-		Main.overview.viewSelector._appsPage.remove_action(this._appsPageClickAction);
-		this._appsPageClickAction = null;
+		/* disconnect click action from the overview */
+		Main.overview._overview._controls.remove_action(this._clickAaction);
+		this._clickAaction = null;
 	}
 }
 
